0d01236a0fd340a6cd0005f4169ea1e281a62bfe
Jose <jose.cabello.ranea@gmail.com>
2023-01-28 13:00:20 +0100

    Generation of Sphere!
    The sphere mesh generation is completed! Though we need to address the color situation (the URP materials, yep, it would've been better to dive into the 3D normal pipeline) and the small bumps in the sphere due to the difference in the distance of each vertice in the initial cube to its center.

---
+++ Procedural Planets/Assets/Scripts/TerrainFace.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainFace
+{
+    Mesh mesh;
+    int resolution;
+    Vector3 localUp;
+    Vector3 axisA; // axisA is a vector 3 that will use the localUp vector to determine the x and z
+    Vector3 axisB; // axisB is a vector 3 that will be the cross product of both axisA and localUp vector
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.localUp = localUp;
+
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        axisB = Vector3.Cross(localUp, axisA);
+    }
+
+    //The resolution will be the numer of vertices around a single edge of the face, so vertices = resolution^2
+    //How many triangles?
+    /* 
+    Imagine resolution = 4
+    * * * *
+    * * * * so, number of faces = (resolution - 1 ) ^ 2
+    * * * * we see that each face (square) is made of two triangles, and each triangle is made of 3 vertices ( a vertice in the scheme is each *)
+    * * * * to that end, number of faces = (resolution - 1) ^ 2 * 2 * 3
+
+    -1, 1, -1
+        *           *
+            ^
+    *       |    * 1, 1, 1       -----> Scheme of the for loop objective
+            |
+            * 0, 0, 0
+
+    */
+
+    public void ConstructMesh()
+    {
+        Vector3[] vertices = new Vector3[resolution * resolution];
+        int [] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
+        int triIndex = 0;
+
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                int i = x + y * resolution;
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+                Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+                vertices[i] = pointOnUnitSphere;
+
+                if (x != resolution - 1 && y != resolution - 1)
+                {
+                    triangles[triIndex] = i;
+                    triangles[triIndex+1] = i + resolution + 1;
+                    triangles[triIndex+2] = i + resolution;
+
+                    triangles[triIndex+3] = i;
+                    triangles[triIndex+4] = i + 1;
+                    triangles[triIndex+5] = i + resolution + 1;
+                    triIndex += 6;
+                }
+            }
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
