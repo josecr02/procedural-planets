ac24f9db3b6418a56160532bf657a21ec5e204f6
Jose <jose.cabello.ranea@gmail.com>
2023-01-30 17:36:15 +0100

    noise to sphere added
    Layers of noise that control the roughness and strength of the noise are added to the project.

--- Procedural Planets/Assets/Scripts/ShapeSettings.cs
+++ Procedural Planets/Assets/Scripts/ShapeSettings.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class ShapeSettings : ScriptableObject
 {
-
     public float planetRadius = 1;
+    public NoiseLayer[] noiseLayers;
+
+    [System.Serializable]
+    public class NoiseLayer
+    {
+        public bool enabled = true;
+        public bool useFirstLayerAsMask;
+        public NoiseSettings noiseSettings;
+    }
 }
