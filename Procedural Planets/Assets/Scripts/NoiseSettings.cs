c59978f2b3a0384f8650f0a0e80cdde1b981120a
Jose <jose.cabello.ranea@gmail.com>
2023-02-02 18:10:27 +0100

    Finished noise filters
    Added multiple noise filters (simple and rigid) to improve the planet's mesh. Also improved rendering the mesh by only rendering selected faces.

--- Procedural Planets/Assets/Scripts/NoiseSettings.cs
+++ Procedural Planets/Assets/Scripts/NoiseSettings.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 [System.Serializable]
 public class NoiseSettings
 {
-    public float strength = 1;
-    [Range(1, 8)]
-    public int numLayers = 1;
-    public float baseRoughness = 1;
-    public float roughness = 2;
-    public float persistence = .5f;
-    public Vector3 centre;
-    public float minValue;
+
+    public enum FilterType { Simple, Ridgid };
+    public FilterType filterType;
+
+    [ConditionalHide("filterType", 0)]
+    public SimpleNoiseSettings simpleNoiseSettings;
+    [ConditionalHide("filterType", 1)]
+    public RigidNoiseSettings ridgidNoiseSettings;
+
+    [System.Serializable]
+    public class SimpleNoiseSettings{
+        public float strength = 1;
+        [Range(1, 8)]
+        public int numLayers = 1;
+        public float baseRoughness = 1;
+        public float roughness = 2;
+        public float persistence = .5f;
+        public Vector3 centre;
+        public float minValue;
+    }
+
+    [System.Serializable]
+    public class RigidNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = .8f;
+    }
 }
--- Procedural Planets/Assets/Scripts/Planet.cs
+++ Procedural Planets/Assets/Scripts/Planet.cs
@@ -9,6 +9,9 @@ public class Planet : MonoBehaviour
     public int resolution =  10;
     public bool autoUpdate = true;
 
+    public enum FaceRenderMask { All, Top, Bottom, Left, Right, Front, Back };
+    public FaceRenderMask faceRenderMask;
+
     public ShapeSettings shapeSettings;
     public ColourSettings colourSettings;
 
@@ -49,6 +52,8 @@ public class Planet : MonoBehaviour
             }
 
             terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+            bool renderFace = faceRenderMask == FaceRenderMask.All || (int)faceRenderMask - 1 == i;
+            meshFilters[i].gameObject.SetActive(renderFace);
         }
     }
 
@@ -79,9 +84,12 @@ public class Planet : MonoBehaviour
 
     void GenerateMesh()
     {
-        foreach (TerrainFace face in terrainFaces)
+        for (int i = 0; i < 6; i++)
         {
-            face.ConstructMesh();
+            if (meshFilters[i].gameObject.activeSelf)
+            {
+                terrainFaces[i].ConstructMesh();
+            }
         }
     }
 
